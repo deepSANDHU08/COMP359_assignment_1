8ec72389ac3404d6e66bd67af787932fdc104786
Pushpdeep.Sandhu <Pushpdeep.sandhu@student.ufv.ca>
2026-01-18 16:58:09 -0800

    change of plans using combination

--- python/main.py
+++ python/main.py
@@ -1,19 +1,37 @@
-U = {"a","b","c","d","e","f"}
+# U = {"a","b","c","d","e","f"}
 
-A = {"a","c"}
-B = {"a","c","e"}
+# A = {"a","c"}
+# B = {"a","c","e"}
 
-def lessEqual(A,B):
-    return A.Issubset(B)
+# def lessEqual(A,B):
+    # return A.issubset(B)
 
-def strictLess(A,B):
+# def strictLess(A,B):
     # means A is a subset of B
-    return A.Issubset(B) and A != B
+    # return A.issubset(B) and A != B
 
-def incomprable(A,B):
-    return (not A.Issubset(B)) and (not B.Issubset(A))
+# def incomprable(A,B):
+    # return (not A.issubset(B)) and (not B.issubset(A))
 
-print(lessEqual({"a"},{"a","b"}))  #true
-print(strictLess({"a","b"},{"a",}))  #false 
-print(incomprable({"a"},{"c"}))   # true (incomprable)
-print(incomprable({"a"},{"a","c"})) #false 
+# print(lessEqual({"a"},{"a","b"}))  #true
+# print(strictLess({"a","b"},{"a",}))  #false 
+# print(incomprable({"a"},{"c"}))   # true (incomprable)
+# print(incomprable({"a"},{"a","c"})) #false '''
+
+from itertools import combinations
+
+U = ["a","b","c","e","f"]
+
+def levels(U):
+    L = []
+    for k in range(len(U) + 1): # this will store
+        level_k = []
+        for comb in combinations(U, k): # for the structure of L
+            level_k.append(frozenset(comb))
+        L.append(level_k)   # <-- moved outside inner loop
+    return L
+
+L = levels(U)
+
+print(len(L))       # should be 6
+print(len(L[3]))    # should be 10
